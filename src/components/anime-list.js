4e188a3eed314c54b3afc45a018fb7a318d2edcf
githubTiagoMarques <al.tiagosoares@gmail.com>
2023-12-03 07:37:32 +0000

    Pagination buttons done and overall styling finished

--- src/components/anime-list.js
+++ src/components/anime-list.js
@@ -33,9 +33,9 @@ const AnimeList = ({ animeList, loading, page, setPage, onFilterChange, filterSe
                             </div>
                         </Link>
                     ))}
-                    <div>
+                    <div className="paginationButtons">
                         <button onClick={() => handlePageChange(page - 1)} disabled={page === 1}>
-                            Previous
+                            Prev
                         </button>
                         <span>{page}</span>
                         <button onClick={() => handlePageChange(page + 1)}>Next</button>
@@ -48,55 +48,113 @@ const AnimeList = ({ animeList, loading, page, setPage, onFilterChange, filterSe
 
 
 const AnimeListStyle = styled.div`
-      @media only screen and (max-width: 790px) {
-        grid-template-columns: repeat(auto-fit, minmax(125px, 1fr));
-        margin: 1rem;
-        .card img{
-            height: 175px !important;
-          }
+
+  @media only screen and (max-width: 790px) {
+    grid-template-columns: repeat(auto-fit, minmax(125px, 1fr));
+    margin: 1rem;
+    .card img {
+      height: 175px !important;
+    }
+  }
+
+  margin: 4rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 2rem;
+
+  .card {
+    position: relative;
+    font-weight: 600;
+    transition: linear 0.3s;
+
+    &:hover {
+      transform: translateY(-10px);
+    }
+
+    .score {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      color: white;
+      background-color: black;
+      padding: 5px 15px;
+      border-radius: 15px;
+    }
+
+    .genres {
+      margin-top: 5px;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 5px;
+
+      div {
+        font-size: 0.8rem;
+        background-color: #c8807c;
+        border-radius: 25px;
+        padding: 5px 10px;
+        color: white;
       }
-      margin: 4rem;
-      display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-      grid-gap: 2rem;
-      .card{
-        position: relative;
-        font-weight: 600;
-        transition: linear 0.3s;
-        &:hover{
-          transform: translateY(-10px);
-        }
-        .score{
-          position: absolute;
-          top: 10px;
-          right: 10px;
-          color: white;
-          background-color: black;
-          padding: 5px 15px;
-          border-radius: 15px;
-        }
-        .genres{
-          margin-top: 5px;
-          display: flex;
-          flex-wrap: wrap;
-          gap: 5px;
-          div{
-            font-size: 0.8rem;
-            background-color: #c8807c;
-            border-radius: 25px;
-            padding: 5px 10px;
-            color: white;
-          }
-        }
-        img{
-          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-          height: 300px;
-          width: 100%;
-          object-fit: cover;
-          border-radius: 15px;
-        }
-      }
+    }
+
+    img {
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      height: 300px;
+      width: 100%;
+      object-fit: cover;
+      border-radius: 15px;
+    }
+  }
+
+  .paginationButtons {
+    display: flex;
+    position: fixed;
+    bottom: 50px;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+    font-weight: 600;
+    background-color: white;
+    border-radius: 6px;
+    left: 50%;
+    transform: translate(-50%, 0);
+    opacity: 0.7;
     
+    &:hover{
+      transition: linear 0.5s;
+      opacity: 1;
+    }
+
+    button {
+      color: #FF3D00;
+      display: inline-block;
+      margin: 5px;
+      outline: 0;
+      cursor: pointer;
+      padding: 5px 16px;
+      font-size: 14px;
+      font-weight: 600;
+      line-height: 20px;
+      border: 1px solid;
+      border-radius: 6px;
+      background-color: #fafbfc;
+      border-color: #1b1f2326;
+      box-shadow: rgba(27, 31, 35, 0.04) 0px 1px 0px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px 0px inset;
+      transition: 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+      transition-property: color, background-color, border-color;
+
+      &:disabled {
+        background-color: whitesmoke;
+        color: #b6b6b6;
+      }
+
+      :hover {
+        background-color: #f3f4f6;
+        border-color: #1b1f2326;
+        transition-duration: 0.1s;
+      }
+    }
+  }
+
 `
 
 const mapStateToProps = state => ({
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -34,13 +34,21 @@ const Navbar = ({ setPage, setFilterSelected, page }) => {
         dispatch(fetchAnimeList('search', 1, searchTerm));
         setSearchPerformed(true);
     };
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <NavbarStyle>
             <Link to="/">
                 <img className="logo" src={logo} alt="Logo" />
             </Link>
-            <div className="right-side">
+            <button className="menu-button" onClick={toggleMenu}>
+                <i className="fa fa-bars"></i>
+            </button>
+            <div className={`right-side ${isMenuOpen ? 'open' : ''}`}>
                 <div
                     className={searchPerformed ? '' : listFilter === 'popular' ? 'selected' : ''}
                     onClick={() => handleFilterClick('popular')}
@@ -72,7 +80,7 @@ const Navbar = ({ setPage, setFilterSelected, page }) => {
                         onChange={(e) => setSearchTerm(e.target.value)}
                         placeholder="Search"
                     />
-                    <button type="submit">Submit</button>
+                    <button type="submit"><i className="fa fa-search"></i></button>
                 </form>
             </div>
         </NavbarStyle>
@@ -81,7 +89,6 @@ const Navbar = ({ setPage, setFilterSelected, page }) => {
 
 const NavbarStyle = styled.div`
   display: flex;
-  flex-wrap: wrap;
   justify-content: space-between;
   position: fixed;
   top: 0;
@@ -91,26 +98,122 @@ const NavbarStyle = styled.div`
   background-color: white;
   z-index: 1000;
   height: 75px;
-  .right-side {
-    padding: 0 4rem;
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    div {
+
+  form {
+    position: relative;
+
+    input {
+      width: 200px;
+      height: 40px;
+      font-size: 16px;
+      padding: 0 20px 0 30px;
+      border-radius: 40px;
+      outline: none;
+      border: solid #EDEDED 2px;
+    }
+
+    button {
+      border: none;
+      position: absolute;
+      border-radius: 25px;
+      zoom: 1.5;
+      background-color: rgba(255, 61, 0, 0.71);
+      color: white;
+      height: 100%;
+      aspect-ratio: 1.5 / 1;
+      right: 0;
+      cursor: pointer;
+    }
+
+  }
+
+  .menu-button {
+    display: none;
+    @media (max-width: 1030px) {
+      margin-right: 3rem;
+      zoom: 1.5;
+      display: block;
+      background: none;
+      border: none;
       font-size: 1.2rem;
-      font-weight: 600;
-      &.selected {
-        color: #FF3D00;
-      }
-      &:hover{
-        color: #FF3D00;
-      }
       cursor: pointer;
     }
   }
+
+  .right-side {
+    padding-right: 4rem;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+
+    div {
+      padding: 0 10px;
+    }
+
+    @media (max-width: 1030px) {
+      display: none;
+      flex-direction: column;
+      position: absolute;
+      top: 75px;
+      left: 0;
+      background-color: white;
+      width: 100%;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      
+      form{
+        order: -1;
+      }
+      
+
+      &.open {
+        display: flex;
+        padding: 1rem 0;
+        justify-content: center;
+      }
+
+      div {
+        width: 100%;
+        padding-bottom: 15px;
+        text-align: center;
+
+        &.selected {
+          color: #FF3D00;
+        }
+
+        &:hover {
+          color: #FF3D00;
+        }
+      }
+    }
+
+    div {
+      font-size: 1.2rem;
+      font-weight: 600;
+
+      &.selected {
+        color: #FF3D00;
+      }
+
+      &:hover {
+        color: #FF3D00;
+      }
+
+      cursor: pointer;
+    }
+  }
+
   .logo {
-    padding: 0 4rem;
+    padding: 0 2rem 0 4rem;
     height: 75px;
+  }
+
+  @media (max-width: 768px) {
+    .logo {
+      padding: 0 1rem;
+    }
+    .menu-button{
+      margin: 0 1.5rem;
+    }
   }
 `;
 
